412e396868fd147f8f9db0da70969254638e2535
davide <davidebotti92@gmail.com>
2023-04-02 18:00:48 +0200

    Tests the translations endpoint

--- webapp/src/main/kotlin/webapp/TestResource.kt
+++ webapp/src/main/kotlin/webapp/TestResource.kt
@@ -4,7 +4,6 @@ import domain.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
@@ -13,8 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestResource(
     private val insertCustomerUseCase: InsertCustomerUseCase,
-    private val findCustomerUseCase: FindCustomerUseCase,
-    private val translationsProvider: TranslationsProvider
+    private val findCustomerUseCase: FindCustomerUseCase
 ) {
 
     @GetMapping("/alive")
@@ -49,17 +47,6 @@ class TestResource(
         return ResponseEntity.ok(
             CustomersResponse(
                 findCustomerUseCase.getAll()
-            )
-        )
-    }
-
-    @GetMapping("/translations/{language}")
-    fun translations(
-        @PathVariable language: String
-    ): ResponseEntity<TranslationsResponse> {
-        return ResponseEntity.ok(
-            TranslationsResponse(
-                translations = translationsProvider.retrieve(language).data
             )
         )
     }
---
+++ webapp/src/test/kotlin/webapp/TranslationsResourceTest.kt
@@ -0,0 +1,40 @@
+package webapp
+
+import com.springexample.utils.Fixtures
+import domain.*
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@WebMvcTest(TranslationsResource::class)
+class TranslationsResourceTest {
+
+    @Autowired
+    private lateinit var mvc: MockMvc
+
+    @MockBean
+    private lateinit var translationsProvider: TranslationsProvider
+
+    private val TRANSLATIONS_RESPONSE = Fixtures.readJson("/translationsResponse.json")
+
+    @Test
+    fun `retrieve translations`() {
+        Mockito.`when`(translationsProvider.retrieve("es")).thenReturn(
+            Translations(
+                mapOf("key1" to "value1", "key2" to "value2")
+            )
+        )
+
+        mvc.perform(
+            get("/translations/es")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().json(TRANSLATIONS_RESPONSE))
+    }
+}
