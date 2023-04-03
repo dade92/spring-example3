c2eece8f3cacbe7793fa07e56d68e4552c904225
dbotti <davide.botti@lastminute.com>
2023-04-03 08:45:17 +0200

    Adds http request. Completes the translation map

--- domain/src/main/kotlin/domain/TranslationsProvider.kt
+++ domain/src/main/kotlin/domain/TranslationsProvider.kt
@@ -14,7 +14,19 @@ class StaticTranslationsProvider : TranslationsProvider {
         Translations(
             mapOf(
                 "appflow.customerData.hi" to "Hi",
-                "appflow.customerData.t_and_c" to "Accept t&c"
+                "appflow.customerData.t_and_c" to "Accept t&c",
+                "appflow.customerData.alias" to "Your alias",
+                "appflow.customerData.actions" to "Actions",
+                "appflow.customerData.submit" to "Submit",
+                "appflow.customerData.undo" to "Undo",
+                "appflow.customerData.areyousure" to "Are you sure???",
+                "appflow.customerData.confirmModal" to "By clicking on confirm you confirm the operation",
+                "appflow.customerData.confirm" to "Confirm",
+                "appflow.customerData.disagree" to "Disagree",
+                "appflow.customerData.thankyoumessage" to "Thanks for your selection ",
+                "appflow.customerData.restart" to "Restart",
+                "appflow.customerData.photo" to "Upload your picture",
+                "appflow.customerData.alertmessage" to "Input must be greater than two letters",
             )
         )
 
