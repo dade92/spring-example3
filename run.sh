a86ee30be38eebc0900aedcbc4f1a79ed2289483
davide <davidebotti92@gmail.com>
2023-04-09 11:42:32 +0200

    Adds run and stop deploy script

---
+++ run.sh
@@ -0,0 +1,16 @@
+echo "Starting database..."
+docker compose up -d mongo
+sleep 2
+
+echo "Starting app..."
+docker compose up -d app
+
+echo "Starting mongo express interface on port 8081..."
+docker compose up -d mongo-express
+sleep 2
+
+docker ps
+
+echo "App seems up and running. Enjoy!"
+
+
