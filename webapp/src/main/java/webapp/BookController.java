5985ec3641677272aa373accb1b02c0b09cd8b7a
Davide Botti <davidebotti92@gmail.com>
2023-08-12 19:29:08 +0200

    Adds simple test

--- webapp/src/main/java/webapp/BookController.java
+++ webapp/src/main/java/webapp/BookController.java
@@ -5,7 +5,6 @@ import graphql.Book;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
