bd7a71082507296e6a7644c0aaa8dbf3628ed63f
Davide Botti <davidebotti92@gmail.com>
2023-11-10 19:37:11 +0100

    Adds the configuration for the FE

--- webapp/src/main/frontend/App.tsx
+++ webapp/src/main/frontend/App.tsx
@@ -4,11 +4,20 @@ interface Props {
     isRunning: boolean
 }
 
+let host = '';
+
+if(process.env.NODE_ENV === 'development') {
+    host = "http://localhost:8080";
+    console.log('Development mode active');
+} else if(process.env.NODE_ENV === 'production') {
+    host = "http://davides-MBP:8080";
+}
+
 export const App: React.FC<Props> = ({isRunning}) => {
     const [alive, setAlive] = useState<boolean>(false);
 
     useEffect(() => {
-        fetch('/api/alive')
+        fetch(`${host}/api/alive`)
             .then((response) => {
                 if(response.status == 200) {
                     setAlive(true);
