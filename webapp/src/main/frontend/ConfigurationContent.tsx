e3bc612da3267e4903139bc5a1195b1136b5c895
Davide Botti <davidebotti92@gmail.com>
2023-11-12 20:20:20 +0100

    React inside the configuration page

--- webapp/src/main/frontend/ConfigurationContent.tsx
+++ webapp/src/main/frontend/ConfigurationContent.tsx
@@ -1,10 +1,11 @@
-import {Typography} from "@mui/material";
+import {Button, Typography} from "@mui/material";
 
 export const ConfigurationContent: React.FC = () => {
 
     return (
         <>
             <Typography>Check the config</Typography>
+            <Button>CLICK ME</Button>
         </>
     )
 
