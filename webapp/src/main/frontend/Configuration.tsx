c82010e20f8e4407b7f0379973ab858c3884aef3
dbotti <davide.botti@lastminute.com>
2023-11-13 22:34:19 +0100

    Remove imports

--- webapp/src/main/frontend/Configuration.tsx
+++ webapp/src/main/frontend/Configuration.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App'
-import {restAliveConfigurationProvider} from "./logic/AliveConfigProvider";
 import {ConfigurationContent} from "./ConfigurationContent";
 
 ReactDOM.render(
