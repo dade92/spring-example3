5087402357ddcf29b63aa0253913959563096d60
Davide Botti <davidebotti92@gmail.com>
2023-11-24 17:36:57 +0100

    Renaming

--- webapp/src/test/java/webapp/AliveControllerTest.java
+++ webapp/src/test/java/webapp/AliveControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import webapp.AliveController;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AliveController.class)
-public class AliveResourceTest {
+public class AliveControllerTest {
 
     @Autowired
     private MockMvc mvc;
