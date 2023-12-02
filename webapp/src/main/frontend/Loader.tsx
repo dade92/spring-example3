17bbf0c295ff94a5d9dd668927d4883744878782
Davide Botti <davidebotti92@gmail.com>
2023-12-02 11:12:50 +0100

    Resolve MR comments

--- webapp/src/main/frontend/Loader.tsx
+++ webapp/src/main/frontend/Loader.tsx
@@ -1,20 +1,19 @@
 import {CircularProgress, circularProgressClasses} from "@mui/material";
 
 export const Loader: React.FC = () => {
-    return (<>
-            <CircularProgress
-                variant="indeterminate"
-                disableShrink
-                sx={{
-                    color: (theme) => (theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8'),
-                    animationDuration: '550ms',
-                    [`& .${circularProgressClasses.circle}`]: {
-                        strokeLinecap: 'round',
-                    },
-                }}
-                size={40}
-                thickness={5}
-            />
-        </>
+    return (
+        <CircularProgress
+            variant="indeterminate"
+            disableShrink
+            sx={{
+                color: (theme) => (theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8'),
+                animationDuration: '550ms',
+                [`& .${circularProgressClasses.circle}`]: {
+                    strokeLinecap: 'round',
+                },
+            }}
+            size={40}
+            thickness={5}
+        />
     )
 }
