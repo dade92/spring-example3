554b588165dd0c0aa30a78225026320ff4ade4e9
Davide Botti <davidebotti92@gmail.com>
2023-12-08 09:54:18 +0100

    Uses card to display the list of events

--- webapp/src/main/frontend/EventList.tsx
+++ webapp/src/main/frontend/EventList.tsx
@@ -1,23 +1,37 @@
 import React from "react";
 import {AppEvent} from './logic/EventDataRetriever'
 import {Loader} from "./Loader";
-import {List, ListItem, ListItemText} from "@mui/material";
+import {Card, CardActionArea, CardContent, List, ListItem, ListItemText, Typography} from "@mui/material";
+import styled from "styled-components";
 
 interface Props {
     events: AppEvent[];
     isLoading: boolean;
 }
 
+const StyledCard = styled(CardContent)`
+  min-height: 56px;
+  display: flex;
+  flex-direction: column;
+  padding: 8px;
+  justify-content: center;
+  align-items: center;
+`
+
 export const EventList: React.FC<Props> = ({events, isLoading}) => {
     return <>
         {isLoading && <Loader/>}
         {
             events.map((e: AppEvent, index) => {
-                return <ListItem data-testid={`event-${index}`}>
-                    <ListItemText>
-                        {e.message}
-                    </ListItemText>
-                </ListItem>
+                return <Card data-testid={`event-${index}`}>
+                    <CardActionArea>
+                        <StyledCard>
+                            <Typography>
+                                {e.message}
+                            </Typography>
+                        </StyledCard>
+                    </CardActionArea>
+                </Card>
             })
         }
     </>
