b6029af139689af112af7ece130def7db09211be
dbotti <davide.botti@lastminute.com>
2023-12-21 15:08:31 +0100

    Remove unused imports

--- webapp/src/main/frontend/Index.tsx
+++ webapp/src/main/frontend/Index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ControlPanel} from './ControlPanel'
-import {restAliveConfigurationProvider} from "./logic/AliveConfigProvider";
-import {restEventsRetriever} from "./logic/EventDataRetriever";
 import {App} from "./App";
 
 ReactDOM.render(
